74619a70c017299286c881aa7134c632151db2cd
roelkers <rufus.oelkers@gmail.com>
2017-11-01 15:55:06 +0100

    flight es2015

--- exercise1/objects/flight-es2015.js
+++ exercise1/objects/flight-es2015.js
@@ -1,5 +1,33 @@
-// TODO: Re-implement flight.js using ES2015
+class MarsFlight{
+  constructor(outbounddate, speed) {
+    this.speed = speed || 1; // default speed is 1
+    this.outbounddate = outbounddate || 'February 19, 2017'; // the departure date
+    this.originplace = 'Earth'; // the departure location
+    this.destinationplace = 'Mars'; // the destination location
+  }
+  duration(){
+    return (63070000000 / this.speed);
+  }
+  durationInDays() {
+    return Math.round(this.duration() / (1000 * 60 * 60 * 24));
+  };
+  getInboundDate() {
+    return new Date(Date.parse(this.outbounddate) + this.duration());
+  };
+};
 
-console.log(`The RyanAir Mars flight will take ca. ${marsFlight.durationInDays()} days (ETA: ${marsFlight.getInboundDate()}).`);
+class FastMarsFlight extends MarsFlight{
+  constructor(outbounddate) {
+    // Call the parent constructor
+    // Fast flights with 3X normal speed
+    super(outbounddate, 3);
+  }
+};
 
-console.log(`Maybe we should upgrade to Elon Emirates and fly there in ${fastMarsFlight.durationInDays()} days.`);
+var plannedDeparture = 'February 20, 2017';
+var marsFlight = new MarsFlight(plannedDeparture);
+var fastMarsFlight = new FastMarsFlight(plannedDeparture);
+
+console.log('The RyanAir Mars flight will take ca. ' + marsFlight.durationInDays() + ' days (ETA: ' + marsFlight.getInboundDate() + ').');
+
+console.log('Maybe we should upgrade to Elon Emirates and fly there in ' + fastMarsFlight.durationInDays() + ' days.');
