0d8e5388ddafc7b71294a74c27c6eec792e11b1b
roelkers <rufus.oelkers@gmail.com>
2017-11-06 10:21:08 +0100

    command line arguments

--- exercise2/nodejs/sum.js
+++ exercise2/nodejs/sum.js
@@ -5,3 +5,10 @@
 // For example:
 // node sum.js 1 2 3
 // 6
+let sum = 0;
+
+for(let i = 2; i < process.argv.length; i++){
+  let p = parseInt(process.argv[i]);
+  sum = sum + p;
+}
+console.log(sum);
