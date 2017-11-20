55768c4f811d62e0a89bfe9d9a4b95e8934f0246
roelkers <rufus.oelkers@gmail.com>
2017-11-20 07:57:27 +0100

    polygon

--- exercise3/polygon.js
+++ exercise3/polygon.js
@@ -3,3 +3,17 @@
  * 1. A constructor that takes an array of integer side lengths.
  * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
  */
+ class Polygon {
+
+    constructor(sides){
+        this.sidelengths = sides;
+        //console.log(sides);
+    }
+    perimeter() {
+        var sum = 0;
+        for(var i =0; i< this.sidelengths.length; i++){
+            sum += this.sidelengths[i];
+        }
+        return sum;
+    }
+}
