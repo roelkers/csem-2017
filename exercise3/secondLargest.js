3d4f2b308bb47be66af8e7d48bb93e54c47c17cf
roelkers <rufus.oelkers@gmail.com>
2017-11-20 07:48:11 +0100

    2nd Largest

--- exercise3/secondLargest.js
+++ exercise3/secondLargest.js
@@ -4,6 +4,19 @@
 *   @return {Number} The second largest number in the array.
 **/
 function getSecondLargest(nums) {
-    // Complete the function
+    //nums = [2,3,3,2,1,4,5,5,4];
 
+    // Complete the function
+    var largest = 0;
+    var sndLargest = 0;
+    for(var i = 0; i<nums.length;i++){
+        if(nums[i]>largest){
+            sndLargest = largest;
+            largest = nums[i];
+        }
+        else if(nums[i]>sndLargest)
+            if(largest != nums[i])
+                sndLargest = nums[i];
+    }
+    return sndLargest;
 }
