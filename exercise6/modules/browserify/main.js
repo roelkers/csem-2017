25539a83d4439f602b5dd7fdb8da7a915f1e1fd7
roelkers <rufus.oelkers@gmail.com>
2017-12-31 17:29:10 +0100

    browserify module bundling

--- exercise6/modules/browserify/main.js
+++ exercise6/modules/browserify/main.js
@@ -1,3 +1,5 @@
 // TODO import common-js style
+var lib = require('../lib/cjs/lib');
+var $ = require('jquery');
 
 $("#test").html("<h2>CJS + Browserify is a " + lib.rating(3) + " experience.</h2>");
--- exercise6/modules/lib/cjs/lib.js
+++ exercise6/modules/lib/cjs/lib.js
@@ -1,9 +1,15 @@
 // CommonJS style
 
-module.exports = function rating(stars) {
+var lib = {};
+
+function rating(stars) {
     if (stars > 3) {
         return "great";
     } else {
         return "nice";
     }
 }
+
+lib.rating = rating;
+
+module.exports = lib;
